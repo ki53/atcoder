dc3c214d371d9e69fa61c10b4e7cdf96a9a83f07
ki53 <ki53jp@gmail.com>
2021-01-31 19:10:11 +0900

    template update

--- tools/templates/template.cpp
+++ tools/templates/template.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true; } return false;}
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true; } return false;}
+template<class T>  inline int ctoi(T& c) {return (int)c - 48; }
+template<class T>  inline char itoc(T& i) {return (char)(i+48); }
 
 const long long INF = 1LL << 60;
 
